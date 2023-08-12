a09876b4ec14009f83000263e0e0e0ed335f59a3
Kazmain Shah <kshah228@illinois.edu>
2023-08-12 18:22:29 -0500

    analytics

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Languages from './ProgrammingLanguages/Languages';
 import Projects from './Projects/Projects';
 import ContactMe from './ContactMe/ContactMe';
 // import NavBar from './NavBar/NavBar';
+import { inject } from '@vercel/analytics';
+ 
 
 
 function App() {
+  inject();
   return (
     <div className="container">
       {/* <NavBar /> */}
